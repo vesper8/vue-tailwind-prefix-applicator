b3513d4e0fb2982e690a8c8cb98b058d00323f38
Sebastien Pelletier <vesperknight@gmail.com>
2020-04-21 18:11:26 +0300

    Added support for TailwindCSS 1.3 Refactored PHP class extractor Classes are now sorted alphabetically

--- extractClassNames.php
+++ extractClassNames.php
@@ -1,73 +1,87 @@
 <?php
 
-// Grab contents of css file
-// $file = file_get_contents('tailwind.css');
-$file = file_get_contents('tailwind-ui.css');
+// Get latest versions of these two files from
+// https://unpkg.com/tailwindcss@%5E1.0/dist/tailwind.css
+// https://cdn.jsdelivr.net/npm/@tailwindcss/ui@latest/dist/tailwind-ui.css
 
-// Strip comments
-$pattern = '!/\*[^*]*\*+([^/][^*]*\*+)*/!';
-$stripped = preg_replace($pattern, '', $file);
+$classes['tailwind.css'] = extractClasses(file_get_contents('tailwind.css'));
+$classes['tailwind-ui.css'] = extractClasses(file_get_contents('tailwind-ui.css'));
 
-// Strip out everything between { and }
-$pattern = '/(?<=\{)(.*?)(?=\})/s';
-$stripped = preg_replace($pattern, '', $stripped);
+function extractClasses($file)
+{
+    // Strip comments
+    $pattern = '!/\*[^*]*\*+([^/][^*]*\*+)*/!';
+    $stripped = preg_replace($pattern, '', $file);
 
-// Remove double line breaks
-$stripped = str_replace("\n\n", "\n", $stripped);
+    // Strip out everything between { and }
+    $pattern = '/(?<=\{)(.*?)(?=\})/s';
+    $stripped = preg_replace($pattern, '', $stripped);
 
-// Convert every line to array
-$classes = explode("\n", $stripped);
+    // Remove double line breaks
+    $stripped = str_replace("\n\n", "\n", $stripped);
 
-$keepers = [];
+    // Convert every line to array
+    $classes = explode("\n", $stripped);
 
-for ($i = 0; $i < count($classes); $i++) {
-    $match = trim($classes[$i]);
+    $keepers = [];
 
-    $match = stripslashes($match);
+    for ($i = 0; $i < count($classes); $i++) {
+        $match = trim($classes[$i]);
 
-    $excludeThesePrefixes = [
-        '.sm:',
-        '.md:',
-        '.lg:',
-        '.xl:',
+        $match = stripslashes($match);
 
-        '.form',
+        $excludeThesePrefixes = [
+            '.sm:',
+            '.md:',
+            '.lg:',
+            '.xl:',
 
-        '.group:hover',
-        '.group:focus',
-    ];
+            '.form',
 
-    if (substr($match, 0, 1) !== '.') {
-        continue;
-    }
+            '.group:hover',
+            '.group:focus',
+        ];
 
-    foreach ($excludeThesePrefixes as $exclude) {
-        if (strpos($match, $exclude) === 0) {
-            continue 2;
+        if (substr($match, 0, 1) !== '.') {
+            continue;
         }
+
+        foreach ($excludeThesePrefixes as $exclude) {
+            if (strpos($match, $exclude) === 0) {
+                continue 2;
+            }
+        }
+
+        $stripThese = [
+            '.',
+            ' {}',
+            ':focus:-ms-input-placeholder',
+            ':focus::-ms-input-placeholder',
+            ':focus::placeholder',
+            '::placeholder',
+            '::-ms-input-placeholder',
+            ':-ms-input-placeholder',
+            '::-moz-placeholder',
+            '::-webkit-input-placeholder',
+            ':focus-within',
+            ':focus',
+            ':hover',
+            ' > :not(template) ~ :not(template)',
+        ];
+
+        foreach ($stripThese as $strip) {
+            $match = str_replace($strip, '', $match);
+        }
+
+        $keepers[] = sprintf('        \'%s\',', $match);
     }
 
-    $stripThese = [
-        '.',
-        ' {}',
-        ':focus:-ms-input-placeholder',
-        ':focus::-ms-input-placeholder',
-        ':focus::placeholder',
-        '::placeholder',
-        '::-ms-input-placeholder',
-        ':-ms-input-placeholder',
-        '::-moz-placeholder',
-        '::-webkit-input-placeholder',
-        ':focus-within',
-        ':focus',
-    ];
-
-    foreach ($stripThese as $strip) {
-        $match = str_replace($strip, '', $match);
-    }
-
-    $keepers[] = sprintf('        \'%s\',', $match);
+    return $keepers;
 }
+
+$keepers = array_merge($classes['tailwind.css'], $classes['tailwind-ui.css']);
+
+sort($keepers);
 
 $file = '
 export default {
@@ -81,4 +95,4 @@ export default {
 };
 ';
 
-file_put_contents('tailwind-classes.js', $file);
+file_put_contents('src/views/tailwind-classes.js', $file);
